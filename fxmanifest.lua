c8fda2128f095667c9d34dac0e4181a17a4a5b8c
Benality <31945990+Benality@users.noreply.github.com>
2024-08-10 07:58:07 -0400

    initital commit

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+game 'rdr3'
+fx_version "adamant"
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+lua54 'yes'
+author 'benneY'
+description 'Bounty script for GRit RP'
+
+
+shared_scripts {
+   
+}
+
+server_scripts {
+  
+}
+
+client_scripts {
+  
+}
+
+files {
+   
+}
+
+-- ui_page 'ui/index.html'
+
+version '0.1.0'
